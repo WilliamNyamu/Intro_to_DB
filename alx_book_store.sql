70a4e6e4c00dfdd47b13daa197b1a23c926ef4a2
WilliamNyamu <williamnyamu08@gmail.com>
2025-09-10 14:16:12 +0300

    db

--- alx_book_store.sql
+++ alx_book_store.sql
@@ -18,7 +18,7 @@ CREATE TABLE Authors (
 
 CREATE TABLE Customers (
     customer_id INT PRIMARY KEY,
-    customer_name  VARCHAR(215),
+    customer_name VARCHAR(215),
     email VARCHAR(215),
     address TEXT
 );
